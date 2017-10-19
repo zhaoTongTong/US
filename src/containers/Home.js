87b94eadbe4e8740370a5dc1759fb9ed158b77e6
ztt <ztt@zttdeMacBook-Pro.local>
2017-10-20 00:08:48 +0800

    添加tag中的头像，todos

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,37 +4,46 @@ import MainFooter from '../components/MainFooter'
 import '../css/home.css'
 
 class Home extends React.Component {
-	render() {
-		return(
-			<div className="homePage">
-				<HomeHeader />
-				<Todo />
-				<Lovetag />
-				<MainFooter />
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="homePage">
+        <HomeHeader/>
+        <Todo/>
+        <Lovetag/>
+        <MainFooter/>
+      </div>
+    )
+  }
 }
 
 class Todo extends React.Component {
-	render() {
-		return (
-			<div className="todo">
-				<span>What is your main focus for today?</span>
-			    <input type="text"/>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="todo">
+        <span>What is your main focus for today?</span>
+        <input type="text"/>
+      </div>
+    )
+  }
 }
 
 class Lovetag extends React.Component {
-	render() {
-		return (
-			<div className="lovetag">
-
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="lovetag">
+        <div className="left">
+          <div className="face"></div>
+          <div className="basicInfo">
+            <div className="nickName">小哥哥</div>
+            <div className="currentFocus">to: 跳槽</div>
+          </div>
+        </div>
+        <div className="right">
+          21313
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Home
