d556143a2147f26fa0b3360d6421f41e918a05b4
ztt <ztt@zttdeMacBook-Pro.local>
2017-10-19 20:58:08 +0800

    添加todos组件和tag组件

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,7 +8,30 @@ class Home extends React.Component {
 		return(
 			<div className="homePage">
 				<HomeHeader />
+				<Todo />
+				<Lovetag />
 				<MainFooter />
+			</div>
+		)
+	}
+}
+
+class Todo extends React.Component {
+	render() {
+		return (
+			<div className="todo">
+				<span>What is your main focus for today?</span>
+			    <input type="text"/>
+			</div>
+		)
+	}
+}
+
+class Lovetag extends React.Component {
+	render() {
+		return (
+			<div className="lovetag">
+
 			</div>
 		)
 	}
