6156a9ab9387c630f964516e481ad51c766754d5
ztt <ztt@zttdeMacBook-Pro.local>
2017-11-20 14:37:34 +0800

    给函数增加参数

--- App.js
+++ App.js
@@ -16,7 +16,11 @@ import TabNavigator from 'react-native-tab-navigator'
 import Toast from 'react-native-easy-toast'
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import Home from './App/Views/Home'
+import Home from './App/Views/Home';
+import Memory from './App/Views/Memory';
+import Message from './App/Views/Message';
+import Add from './App/Views/Add';
+import Setting from './App/Views/Setting';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -34,7 +38,7 @@ export default class App extends Component<{}> {
     }
 
     //导航器函数
-    _renderTabarItems(selectedTab,icon,title){
+    _renderTabarItems(selectedTab, icon, title, Component){
         return (
             <TabNavigator.Item
                 //设置选中的位置
@@ -52,14 +56,7 @@ export default class App extends Component<{}> {
                 //点击Event
                 onPress={() => this.setState({ selectedTab: selectedTab })}
             >
-            <View style={styles.page}>
-                {
-                    selectedTab === "home" ?
-                        <Home />
-                         :
-                        <Text style={{fontSize:18,padding:15,color: '#999',textAlign: 'center'}}>{title}</Text>
-                }
-            </View>
+            <Component />
             </TabNavigator.Item>
         )
     }
@@ -68,11 +65,11 @@ export default class App extends Component<{}> {
     return (
         <View style={styles.container}>
           <TabNavigator>
-              {this._renderTabarItems('home', "md-home", "首页")}
-              {this._renderTabarItems('story', "md-images", "故事")}
-              {this._renderTabarItems('memory', "md-heart", "纪念日")}
-              {this._renderTabarItems('message', "md-chatboxes", "留言")}
-              {this._renderTabarItems('setting', "ios-person", "我的")}
+              {this._renderTabarItems('home', "md-home", "首页", Home)}
+              {this._renderTabarItems('story', "md-images", "故事", Add)}
+              {this._renderTabarItems('memory', "md-heart", "纪念日", Memory)}
+              {this._renderTabarItems('message', "md-chatboxes", "留言", Message)}
+              {this._renderTabarItems('setting', "ios-person", "我的", Setting)}
           </TabNavigator>
           <Toast position={"center"} ref="toast"/>
       </View>
